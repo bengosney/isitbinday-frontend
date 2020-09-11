be8e0bfa2b35a4aa074e4fcedd1d8a4232cdab97
Ben Gosney <bengosney@googlemail.com>
2020-09-11 08:58:33 +0100

    Working task list actions

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,5 +1,3 @@
 export const UCFirst = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 export const CammelToTitle = (s) => UCFirst(s).replace(/([a-z0-9])([A-Z])/g, (match, p1, p2) => `${p1} ${p2}`);
 export const SnakeToTitle = (s) => UCFirst(s).replace(/_([A-Z])/gi, (match, p1) => ` ${UCFirst(p1)}`);
-
-console.log(CammelToTitle(SnakeToTitle('date_dueBob')));
--- src/widgets/TaskList.js
+++ src/widgets/TaskList.js
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
-import { useApiFetch } from '../utils/apiFetch';
-import { Button, Pagination, Menu, List } from 'semantic-ui-react';
+import apiFetch, { useApiFetch } from '../utils/apiFetch';
+import { Button, Pagination, Menu, List, Grid } from 'semantic-ui-react';
 
-const TaskList = ({ refreshKey = 0, limit = 25, offset = 0 }) => {
+const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
   const [currentOffset, setCurrentOffset] = useState(offset);
+  const [currentRefreshKey, setCurrentRefreshKey] = useState(refreshKey);
+  const refresh = () => setCurrentRefreshKey(currentRefreshKey + 1);
 
-  const data = useApiFetch(`api/tasks/?limit=${limit}&offset=${currentOffset}`, null, refreshKey);
+  const data = useApiFetch(`api/tasks/?limit=${limit}&offset=${currentOffset}`, null, currentRefreshKey);
 
   if (data == null) {
     return <div>Loading</div>;
@@ -16,11 +18,28 @@ const TaskList = ({ refreshKey = 0, limit = 25, offset = 0 }) => {
   const pages = Math.ceil(count / limit);
   const currentPage = currentOffset / limit + 1;
 
+  const callAction = (id, action) => apiFetch(`api/tasks/${id}/${action}`).then(() => refresh());
+  const makeButtons = (id, actions) =>
+    actions.map((action) => (
+      <Button size='mini' key={action} onClick={() => callAction(id, action)}>
+        {action}
+      </Button>
+    ));
+
   return (
     <React.Fragment>
       <List>
-        {results.map(({ id, title, effort }) => (
-          <List.Item key={id} header={title} content={`Effort: ${effort || '-'}`} />
+        {results.map(({ id, title, effort, state, available_state_transitions }) => (
+          <List.Item key={id}>
+            <List.Header>{title}</List.Header>
+            <List.Content>
+              <Grid columns="equal">
+                <Grid.Column width={2}>{`State: ${state}`}</Grid.Column>
+                <Grid.Column width={2}>{`Effort: ${effort || '-'}`}</Grid.Column>
+                <Grid.Column>{makeButtons(id, available_state_transitions)}</Grid.Column>
+              </Grid>
+            </List.Content>
+          </List.Item>
         ))}
       </List>
       <Pagination
