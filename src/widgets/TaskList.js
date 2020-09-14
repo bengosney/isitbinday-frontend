3832949c9a2fcd50e55b86aaef7b74a8a900fbcf
Ben Gosney <bengosney@googlemail.com>
2020-09-14 09:04:19 +0100

    make the list drag and drop, doesn't stick tho

--- src/widgets/TaskList.js
+++ src/widgets/TaskList.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
 import apiFetch, { useApiFetch } from '../utils/apiFetch';
 import { Button, Pagination, Menu, List, Grid } from 'semantic-ui-react';
+import { UCFirst } from '../utils/string';
+
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
   const [currentOffset, setCurrentOffset] = useState(offset);
@@ -21,27 +24,36 @@ const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
   const callAction = (id, action) => apiFetch(`api/tasks/${id}/${action}`).then(() => refresh());
   const makeButtons = (id, actions) =>
     actions.map((action) => (
-      <Button size='mini' key={action} onClick={() => callAction(id, action)}>
-        {action}
+      <Button size="mini" key={action} onClick={() => callAction(id, action)}>
+        {UCFirst(action)}
       </Button>
     ));
 
   return (
     <React.Fragment>
-      <List>
-        {results.map(({ id, title, effort, state, available_state_transitions }) => (
-          <List.Item key={id}>
-            <List.Header>{title}</List.Header>
-            <List.Content>
-              <Grid columns="equal">
-                <Grid.Column width={2}>{`State: ${state}`}</Grid.Column>
-                <Grid.Column width={2}>{`Effort: ${effort || '-'}`}</Grid.Column>
-                <Grid.Column>{makeButtons(id, available_state_transitions)}</Grid.Column>
-              </Grid>
-            </List.Content>
-          </List.Item>
-        ))}
-      </List>
+      <DragDropContext onDragEnd={(e) => console.log('drag end', e)}>
+        <Droppable droppableId="droppable">
+          {(provided, snapshot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {results.map(({ id, title, effort, state, available_state_transitions, position }, index) => (
+                <Draggable key={id} draggableId={`${id}`} index={index}>
+                  {(provided, snapshot) => (
+                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <div>{title}</div>
+                      <div>
+                        <span>{`State: ${state}`}</span>
+                        <span>{`Effort: ${effort || '-'}`}</span>
+                        <span>{makeButtons(id, available_state_transitions)}</span>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       <Pagination
         activePage={currentPage}
         totalPages={pages}
@@ -57,3 +69,7 @@ const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
 };
 
 export default TaskList;
+
+/*
+
+*/
