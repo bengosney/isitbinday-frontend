84144fd40ad44d2f3ff6ca1b198db8ba18c66173
Ben Gosney <bengosney@googlemail.com>
2020-09-16 09:06:39 +0100

    Muilty col

--- src/widgets/TaskList.js
+++ src/widgets/TaskList.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import apiFetch, { useApiFetch } from '../utils/apiFetch';
 import { Button, Pagination, Card } from 'semantic-ui-react';
 import { UCFirst } from '../utils/string';
-import { Stack } from '@chakra-ui/core';
+import { Stack, Grid, Text, Box } from '@chakra-ui/core';
 
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
@@ -16,6 +16,7 @@ const reorder = (list, startIndex, endIndex) => {
 
 const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
   const [currentOffset, setCurrentOffset] = useState(offset);
+  const [states, setStates] = useState([]);
   const [tasks, setTasks] = useState(null);
   const [count, setCount] = useState(0);
   const [currentRefreshKey, setCurrentRefreshKey] = useState(refreshKey);
@@ -30,6 +31,14 @@ const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
       setTasks(results);
     }
   }, [data]);
+
+  const statesResponse = useApiFetch(`api/tasks/kanbanStates/`);
+  useEffect(() => {
+    if (statesResponse !== null) {
+      const { states: _states = [] } = statesResponse;
+      setStates(_states);
+    }
+  }, [statesResponse]);
 
   const pages = Math.ceil(count / limit);
   const currentPage = currentOffset / limit + 1;
@@ -54,7 +63,6 @@ const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
     const items = reorder(tasks, result.source.index, result.destination.index);
 
     let pos = 1;
-    //const positions = items.map((i) => ({ id: i.id, position: pos++ }));
     const positions = items.map((i) => ({ id: i.id, position: pos++ }));
 
     apiFetch(`api/tasks/position/`, { positions });
@@ -64,6 +72,40 @@ const TaskList = ({ refreshKey = 0, limit = 15, offset = 0 }) => {
 
   return (
     <React.Fragment>
+      <DragDropContext onDragEnd={(e) => dragEnd(e)}>
+        <Grid templateColumns={`repeat(${states.length}, 1fr)`} gap={6}>
+          {states.map((state) => (
+            <Stack>
+              <Text>{state}</Text>
+              <Droppable key={state} droppableId={state}>
+                {(provided, snapshot) => (
+                  <Stack {...provided.droppableProps} ref={provided.innerRef}>
+                    {tasks.map(
+                      ({ id, title, effort, state: taskState, available_state_transitions, position }, index) => {
+                        if (state !== taskState) {
+                          return null;
+                        }
+                        return (
+                          <Draggable key={id} draggableId={`${id}`} index={index}>
+                            {(provided, snapshot) => (
+                              <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                                <Box key={`${state}-${id}`} border="1px solid lightgray" padding={5}>
+                                  <Text>{title}</Text>
+                                </Box>
+                              </div>
+                            )}
+                          </Draggable>
+                        );
+                      }
+                    )}
+                    {provided.placeholder}
+                  </Stack>
+                )}
+              </Droppable>
+            </Stack>
+          ))}
+        </Grid>
+      </DragDropContext>
       <DragDropContext onDragEnd={(e) => dragEnd(e)}>
         <Droppable droppableId="droppable">
           {(provided, snapshot) => (
