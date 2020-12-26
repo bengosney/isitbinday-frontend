bde5791a77b4fb6d58ee588e3db04946532380b2
Ben Gosney <bengosney@googlemail.com>
2020-12-26 22:36:54 +0000

    Rewrite the fetch and auth for better handling of tokens

--- src/Auth.js
+++ src/Auth.js
@@ -1,82 +1,54 @@
-import React, { useReducer } from 'react';
-import apiFetch from './utils/apiFetch';
+import React, { useReducer, useEffect } from 'react';
+import apiFetch, { login as doLogin, logout as doLogout, checkLogin } from './utils/apiFetch';
 
 export const authContext = React.createContext({ loggedIn: false });
 
-export const clearAuth = () => {
-  localStorage.removeItem('token');
-  localStorage.removeItem('refresh');
-};
-
-export const checkAuth = () => {
-  const refresh = localStorage.getItem('refresh');
-
-  return refresh !== null;
-};
-
-export const refreshToken = async () => {
-  if (refreshToken.isRefreshing === false) {
-    refreshToken.isRefreshing = true;
-    console.log('new refresh promise');
-    refreshToken.refreshPromise = new Promise((resolve, reject) => {
-      (async () => {
-        const refresh = localStorage.getItem('refresh');
-
-        if (refresh === null) {
-          console.log('No refresh token', refresh);
-          reject('No refresh token');
-        }
-
-        clearAuth();
-
-        const res = await apiFetch('api/token/refresh/', { refresh });
-
-        localStorage.setItem('token', res.access);
-        localStorage.setItem('refresh', res.refresh);
-
-        refreshToken.isRefreshing = false;
-        resolve(true);
-      })();
-    });
-  }
-
-  return refreshToken.refreshPromise;
-};
-
-refreshToken.refreshPromise = null;
-refreshToken.isRefreshing = false;
-
 const Auth = ({ children }) => {
-  const ACTION_LOGGEDIN = 'loggedIn';
-  const ACTION_LOGGEDOUT = 'loggedOut';
+  const ACTION_LOGGED_IN = 'loggedIn';
+  const ACTION_LOGGED_OUT = 'loggedOut';
 
   const { Provider } = authContext;
 
   const [loginState, loginDispatch] = useReducer(
     (state, action) => {
       switch (action.type) {
-        case ACTION_LOGGEDIN:
-          localStorage.setItem('token', action.tokens.access);
-          localStorage.setItem('refresh', action.tokens.refresh);
-          apiFetch('api/tasks/auto_archive/');
+        case ACTION_LOGGED_IN:
+          apiFetch('api/tasks/tasks/auto_archive/');
           return { loggedIn: true };
-        case ACTION_LOGGEDOUT:
-          localStorage.removeItem('token');
-          localStorage.removeItem('refresh');
+        case ACTION_LOGGED_OUT:
           return { loggedIn: false };
         default:
           throw new Error(`${action.type} not supported`);
       }
     },
-    { loggedIn: localStorage.getItem('token') != null }
+    { loggedIn: checkLogin() }
   );
 
-  const login = (username, password) => {
-    apiFetch('api/token/', { username: username, password: password }).then((res) =>
-      loginDispatch({ type: ACTION_LOGGEDIN, tokens: res })
-    );
+  useEffect(() => {
+    const localStorageUpdated = () => {
+      if (!checkLogin()) {
+        loginDispatch({ type: ACTION_LOGGED_OUT });
+      }
+    };
+
+    window.addEventListener('storage', localStorageUpdated);
+
+    return () => window.removeEventListener('storage', localStorageUpdated);
+  }, []);
+
+  const login = async (username, password) => {
+    try {
+      await doLogin(username, password);
+      loginDispatch({ type: ACTION_LOGGED_IN });
+    } catch (err) {
+      loginDispatch({ type: ACTION_LOGGED_OUT });
+    }
   };
-  const logout = () => loginDispatch({ type: ACTION_LOGGEDOUT });
+
+  const logout = () => {
+    doLogout();
+    loginDispatch({ type: ACTION_LOGGED_OUT });
+  };
 
   return <Provider value={{ loggedIn: loginState.loggedIn, login, logout }}>{children}</Provider>;
 };
--- src/utils/apiFetch.js
+++ src/utils/apiFetch.js
@@ -1,10 +1,114 @@
-import { refreshToken, clearAuth } from '../Auth';
 import { useState, useEffect } from 'react';
 
 export const origin = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
-const _apiFetch = async (url, args = null, method = null) => {
-  const token = localStorage.getItem('token');
+const setLocalStorage = (key, value) => {
+  localStorage.setItem(key, value);
+  window.dispatchEvent(new Event('storage'));
+};
+
+const removeLocalStorage = (key) => {
+  localStorage.removeItem(key);
+  window.dispatchEvent(new Event('storage'));
+};
+
+const fetchFromOrigin = async (resource, init = {}) => {
+  const cleanResource = `${resource}`.replace(`${origin}/`, '');
+  return fetch(`${origin}/${cleanResource}`, init);
+};
+
+const fetchJsonFromOrigin = async (resource, init = {}) => {
+  const response = await fetchFromOrigin(resource, init);
+
+  if (response.status !== 200) {
+    throw new Error(response);
+  }
+
+  return response.json();
+};
+
+const clearAuth = () => {
+  removeLocalStorage('token');
+  removeLocalStorage('refresh');
+};
+
+const getRefresh = () => {
+  return localStorage.getItem('refresh');
+};
+
+const setRefresh = (token) => {
+  setLocalStorage('refresh', token);
+};
+
+const getToken = () => {
+  return localStorage.getItem('token');
+};
+
+const setToken = (token) => {
+  setLocalStorage('token', token);
+};
+
+const checkRefresh = () => {
+  const refresh = getRefresh();
+  return refresh !== null && typeof refresh !== 'undefined';
+};
+
+export const refreshToken = async () => {
+  if (refreshToken.isRefreshing === false) {
+    refreshToken.isRefreshing = true;
+    refreshToken.refreshPromise = new Promise((resolve, reject) => {
+      (async () => {
+        const refresh = getRefresh() || null;
+
+        if (refresh == null) {
+          return reject('No refresh token');
+        }
+
+        const options = getOptions({ refresh });
+
+        const res = await fetchFromOrigin('api/token/refresh/', options);
+        if (res.status != 200) {
+          clearAuth();
+          return reject(`${res.status}`);
+        }
+
+        const json = await res.json();
+
+        setToken(json.access);
+        setRefresh(json.refresh);
+        return resolve(true);
+      })();
+    })
+      .then(() => (refreshToken.isRefreshing = false))
+      .then(() => true);
+  }
+
+  return refreshToken.refreshPromise;
+};
+
+refreshToken.refreshPromise = null;
+refreshToken.isRefreshing = false;
+
+export const login = async (username, password) => {
+  const options = getOptions({ username: username, password: password });
+  const res = await fetchJsonFromOrigin('api/token/', options);
+
+  setToken(res.access);
+  setRefresh(res.refresh);
+
+  return true;
+};
+
+export const logout = async () => {
+  clearAuth();
+};
+
+export const checkLogin = () => {
+  return checkRefresh();
+};
+
+const getOptions = (args = null, method = null) => {
+  const token = getToken();
 
   const options =
     args != null
@@ -25,23 +129,26 @@ const _apiFetch = async (url, args = null, method = null) => {
     options.headers.Authorization = `Bearer ${token}`;
   }
 
-  const cleanUrl = `${url}`.replace(`${origin}/`, '');
-
-  return fetch(`${origin}/${cleanUrl}`, options);
+  return options;
 };
 
 const apiFetch = async (url, args = null) => {
   const method = parseInt(args?.id || 0) > 0 ? 'put' : null;
+  const options = getOptions(args, method);
 
-  let res = await _apiFetch(url, args, method);
+  let res = await fetchFromOrigin(url, options);
   if (res.status === 401) {
-    console.log('trying refresh');
-    const refreshed = await refreshToken();
+    let refreshed = false;
+    try {
+      refreshed = await refreshToken();
+    } catch (err) {
+      refreshed = false;
+    }
+
     if (refreshed) {
-      console.log('retrying request', url);
-      res = await _apiFetch(url, args, method);
+      options.headers.Authorization = `Bearer ${getToken()}`;
+      res = await fetchFromOrigin(url, options);
     } else {
-      console.log('failed refesh');
       clearAuth();
     }
   }
