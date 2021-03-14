7f7b9d752cea3b6bfec2e605531cd6e1addd6318
Ben Gosney <bengosney@googlemail.com>
2021-03-14 21:21:16 +0000

    Add config and build id

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
-import { ChakraProvider, Box } from '@chakra-ui/react';
+import { ChakraProvider, Box, Text } from '@chakra-ui/react';
 import customTheme from './theme';
 
 import Auth from './Auth';
 import MainAppSection from './sections/MainAppSection';
+import { getConfig } from './config';
 
 function App() {
   return (
@@ -17,6 +18,11 @@ function App() {
             <MainAppSection />
           </Router>
         </Auth>
+      </Box>
+      <Box maxW="90vw" margin="auto" w="100%" paddingTop={4} paddingBottom={4}>
+        <Text fontSize="xs" fontFamily="Monaco, Lucida Console, Courier New, Courier">
+          Build: {getConfig('build')}
+        </Text>
       </Box>
     </ChakraProvider>
   );
