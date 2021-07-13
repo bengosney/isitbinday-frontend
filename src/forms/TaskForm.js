e1a1930e7d42c1bf37e15b6b72a35b5c77b914af
Ben Gosney <bengosney@googlemail.com>
2021-07-13 09:01:48 +0100

    Hide unused fields

--- src/forms/TaskForm.js
+++ src/forms/TaskForm.js
@@ -45,8 +45,8 @@ const TaskForm = ({ details, postSave = null }) => {
       <Form.Input name="title" />
       <Form.DateField name="due_date" />
       <Form.Input name="repeats" />
-      <Form.Input name="effort" />
-      <Form.Input name="blocked_by" />
+      <Form.Input display="none" name="effort" />
+      <Form.Input display="none" name="blocked_by" />
       <Form.Button type={'submit'}>Save</Form.Button>
     </Form>
   );
