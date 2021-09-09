62fcebec32f7a42297a51555be628f5f95609cb1
Ben Gosney <bengosney@googlemail.com>
2021-09-09 21:56:11 +0100

    Basic recipe display

---
+++ src/sections/RecipeSection.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { useRouteMatch, Route, Switch, useHistory } from 'react-router-dom';
+import BookList from '../widgets/BookList';
+import FAB from '../widgets/FAB';
+import { MdAdd } from 'react-icons/md';
+import { Heading, Stack } from '@chakra-ui/react';
+import usePageTitle from '../utils/usePageTitle';
+import RecipeList from '../widgets/RecipeList';
+import RecipeDetails from '../widgets/RecipeDetails';
+
+const RecipeSection = () => {
+  usePageTitle('Recipes');
+  const { path } = useRouteMatch();
+
+  const getUrl = (slug) => {
+    return `${path}/${slug}`.replace('//', '/');
+  };
+
+  const listUrl = getUrl('');
+  const viewUrl = getUrl('/:slug');
+  const addUrl = getUrl('add');
+  const newUrl = getUrl('new');
+
+  const history = useHistory();
+
+  return (
+    <React.Fragment>
+      <Heading>Recipes</Heading>
+      <Switch>
+        <Route path={viewUrl}>
+            <Stack my={6}>
+                <RecipeDetails />
+            </Stack>
+        </Route>
+        <Route path={listUrl}>
+          <Stack my={6}>
+            <RecipeList />
+          </Stack>
+        </Route>
+      </Switch>
+    </React.Fragment>
+  );};
+
+export default RecipeSection;
---
+++ src/widgets/RecipeDetails.js
@@ -0,0 +1,46 @@
+import React, { useCallback, useEffect, useReducer } from 'react';
+import apiFetch, { useApiFetch } from '../utils/apiFetch';
+import { List, ListItem, ListIcon, OrderedList, UnorderedList, Text } from '@chakra-ui/react';
+import { Table, Tbody, Tr, Th, Td, TableCaption, Thead } from '@chakra-ui/react';
+import { BiFoodMenu, BiUser } from 'react-icons/bi';
+import { useRouteMatch, Route, Switch, useHistory, useParams, Link } from 'react-router-dom';
+
+const RecipeDetails = () => {
+  const { slug } = useParams();
+  const details = useApiFetch(`api/recipes/recipe/${slug}`);
+
+  if (details === null) {
+    return <div>Loading...</div>;
+  }
+  console.log('details', details);
+  return (
+    <div>
+      <h1>{details.name}</h1>
+      <div>{details.description}</div>
+
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Ingredient</Th>
+            <Th>Amount</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {details.ingredients.map((ingredient) => (
+            <Tr key={ingredient.id}>
+              <Td>{ingredient.name}</Td>
+              <Td>{ingredient.qty}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+      <ol>
+        {details.steps.map((step) => (
+          <li key={step.id}>{step.description}</li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+export default RecipeDetails;
