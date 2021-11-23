afc8ce5db50d00b658a8bd1f1ba8988a85555fff
Ben Gosney <bengosney@googlemail.com>
2021-11-23 21:46:03 +0000

    Add pagination to the book list

--- src/widgets/BookList.js
+++ src/widgets/BookList.js
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useReducer, useState } from 'react';
 import apiFetch, { useApiFetch } from '../utils/apiFetch';
-import { List, ListItem, ListIcon, Input, Text, useToast } from '@chakra-ui/react';
+import { List, ListItem, ListIcon, Input, Text, useToast, Button, Stack, HStack } from '@chakra-ui/react';
 import { BiBook, BiUser } from 'react-icons/bi';
 
 import useDebounced from '../utils/useDebounced';
@@ -11,10 +11,18 @@ import BarcodeInput from './BarcodeInput';
 import error from '../sounds/error';
 import success from '../sounds/success';
 
-const BookList = () => {
+const BookList = ({page = 0}) => {
+  const npp = 50;
+
+  const [offset, setOffset] = useState(page * npp);
+  const [count, setCount] = useState(0);
+
+  const currentPage = Math.floor(offset / npp);
+  const totalPages = Math.ceil(count / npp);
+
   const [debouncedSearch, setSearch, search] = useDebounced('');
 
-  const fetchUrl = `api/books/book/?limit=1000&search=${debouncedSearch}`;
+  const fetchUrl = `api/books/book/?limit=${npp}&offset=${offset}&search=${debouncedSearch}`;
   const apiResults = useApiFetch(fetchUrl);
   const [results, setResults] = useState(apiResults);
   const { results: books = [] } = results || {};
@@ -38,6 +46,9 @@ const BookList = () => {
 
   useEffect(() => {
     setResults(apiResults);
+    if (apiResults !== null) {
+      setCount(apiResults.count);
+    }
   }, [apiResults]);
 
   const onScan = (barcode) => {
@@ -63,6 +74,15 @@ const BookList = () => {
           </ListItem>
         ))}
       </List>
+      <HStack hidden={totalPages < 1}>
+        <Button onClick={() => setOffset((o) => o - npp)} disabled={offset - npp < 0}>
+          Prev
+        </Button>
+        <Text>{currentPage + 1} of {totalPages}</Text>
+        <Button onClick={() => setOffset((o) => o + npp)} disabled={offset + npp >= count}>
+          Next
+        </Button>
+      </HStack>
       <BarcodeInput onScan={(barcode) => onScan(barcode)} />
     </>
   );
