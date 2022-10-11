3061d775159b503317b462d6eb3802592a4b4baa
Ben Gosney <bengosney@googlemail.com>
2022-10-11 09:30:38 +0100

    needed to be wider

--- src/widgets/MaxWidth.js
+++ src/widgets/MaxWidth.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 
 const MaxWidth = ({ children, ...props }) => (
-  <Box maxWidth="1024px" margin="auto" paddingX={4} {...props}>
+  <Box maxWidth={{base: "100%", md: "90vw"}} margin="auto" paddingX={4} {...props}>
     {children}
   </Box>
 );
