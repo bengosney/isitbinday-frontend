620ad8c87618de9f9d0b3e2077b06e0439d41baf
Ben Gosney <bengosney@googlemail.com>
2025-10-28 19:40:33 +0000

    feat: update ESLint configuration and add TypeScript support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,20 @@
 import pluginJs from '@eslint/js';
+import pluginTs from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
 import globals from 'globals';
 
 export default [
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
-  { languageOptions: { globals: globals.browser } },
+  { files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'] },
+  { languageOptions: { parser: parserTs, globals: globals.browser } },
   pluginJs.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginTs.configs.recommended,
   {
     rules: {
       'react/prop-types': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
   {
