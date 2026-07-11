f471f207cb88a35b323870a24edb7941daacacaa
Ben Gosney <bengosney@googlemail.com>
2026-07-11 09:33:33 +0100

    feat: switch to Instrument Sans and JetBrains Mono fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import App from './App';
 import './index.css';
-import './reset.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <App />
-);
+createRoot(document.getElementById('root')!).render(<App />);
