8b889bf88ce474098d1b52c25a76ef0262ba54aa
Ben Gosney <bengosney@googlemail.com>
2026-07-12 19:44:07 +0100

    chore: migrate infrastructure from dokku to aws amplify
    Removes the dokku Terraform resources, Docker Makefile, and app.json, updates Amplify env vars for Vite (VITE_API_URL), and bumps Node to 22 in .nvmrc.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "~> 4.0"
     }
-    dokku = {
-      source  = "aliksend/dokku"
-      version = "~> 1.0.14"
-    }
   }
 
   backend "s3" {
@@ -27,20 +23,8 @@ variable "cloudflare_api_token" {
   description = "Cloudflare API token"
 }
 
-variable "hosting_domain" {
-  description = "Hosting domain"
-}
-
 variable "zoneid" {
   description = "Cloudflare zone ID"
-}
-
-variable "image" {
-  description = "Docker image"
-}
-
-provider "dokku" {
-  ssh_host = var.hosting_domain
 }
 
 provider "cloudflare" {
